bdad803f81f1b262721fcf437bea10f13dcc2365
Xoftion <xb5366708@gmail.com>
2025-07-31 02:13:23 +0100

    Update api.ts

--- frontend/src/lib/backend/api.ts
+++ frontend/src/lib/backend/api.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://giabrok.onrender.com";
-
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://giabrok.onrender.com";
 export interface Container {
   id: string;
   name: string;
