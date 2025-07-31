dd6dbc025998d7c2b1ae6f0b07381e5816deec45
Xoftion <xb5366708@gmail.com>
2025-07-31 03:01:06 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "Starting backend server..."
-cp config.ts backend/config.ts
-cd backend && bun run build && bun src/index.ts &
+cd /app/backend
+bun src/index.ts &
 
 echo "Starting frontend server..."
-cd frontend && bun run build && bun run start &
+cd /app/frontend
+bun run start &
 
-echo "Both servers are running..."
+echo "Both servers are started..."
 echo "Press Ctrl+C to stop both servers"
 
 wait
